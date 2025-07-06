26ce1b6c54d1f1a96741ba1427565b795d46dbda
Hloni <hloniletsaba@gmail.com>
2025-07-06 18:46:58 +0200

    first commit

---
+++ src/gradebooksystem/GradeBookSystem.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gradebooksystem;
+
+/**
+ *
+ * @author hloni
+ */
+public class GradeBookSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         mainFrame frmM = new mainFrame();
+        frmM.setVisible(true);
+    }
+    
+}
